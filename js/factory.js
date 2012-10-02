ee90f4478d54ef409a1cb209a4632a56dc7a1cf6
Nils Hörrmann <post@nilshoerrmann.de>
2012-10-02 17:08:18 +0200

    Relative times

--- js/factory.js
+++ js/factory.js
@@ -42,6 +42,61 @@ var Factory;
 		},
 		
 	/*-------------------------------------------------------------------------
+		Network
+	-------------------------------------------------------------------------*/
+		
+		getRelativeTime: function() {
+			var time = $(this);
+			
+			// Get relative time distance
+			time.text(Factory.parseTime(time.attr('datetime')));
+		},
+
+		parseTime: function(string) {
+			var from = Date.parse(string),
+				to = new Date().getTime(),
+				distance = to - from;
+
+			// Convert time to minutes
+			time = Math.floor(distance / 60000);
+
+			// Return relative time
+			if(time < 1) {
+				return 'just now';
+			}
+			if(time < 2) {
+				return 'a minute ago';
+			}
+			if(time < 45) {
+				return time + 'minutes ago';
+			}
+			if(time < 90) {
+				return 'about 1 hour ago';
+			}
+			if(time < 1440) {
+				return 'about' + Math.floor(time / 60) + ' hours ago';
+			}
+			if(time < 2880) {
+				return '1 day about';
+			}
+			if(time < 43200) {
+				return Math.floor(time / 1440) + ' days ago';
+			}
+			if(time < 86400) {
+				return 'about a month ago';
+			}
+			if(time < 525960) {
+				return Math.floor(time / 43200) + ' months ago';
+			}
+			if(time < 1051199) {
+				return 'about a year ago';
+			}
+			else {
+				return 'over ' + Math.floor(time / 525960) + ' years ago';
+			}
+		},
+		
+	/*-------------------------------------------------------------------------
 		Storage
 	-------------------------------------------------------------------------*/
 	
@@ -82,7 +137,9 @@ var Factory;
 		$('#network')
 			.on('mouseenter.factory', Factory.showNetwork)
 			.on('mouseleave.factory', Factory.hideNetwork);
-
+			
+		// Set relative time
+		$('time.relative').each(Factory.getRelativeTime);
 	});
 	
 })(jQuery.noConflict());
